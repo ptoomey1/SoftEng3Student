40d549f8845f306b126121bec8d50bcada6db001
ptoomey1 <35386@DESKTOP-31QR3UF.nuigalway.ie>
2021-11-04 00:55:42 +0000

    Commit 1 for part b

--- src/main/java/SoftEng1/Student.java
+++ src/main/java/SoftEng1/Student.java
@@ -100,11 +100,9 @@ public boolean removeModule(Module module){
     return modules.remove(module);
     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    
+    //public static void main(String[] args) {
+       
+    //}
     
 }
